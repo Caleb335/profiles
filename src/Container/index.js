6b20f91b5f610e7b4c766e32dd72fe4fbfaa7ae9
Caleb Olojo <belac335@gmail.com>
2021-02-11 11:28:32 +0100

    feat: refactor profile cards

--- src/Container/index.js
+++ src/Container/index.js
@@ -30,36 +30,41 @@ const Container = () => {
 
   return (
     <section className={style.root}>
-      {loading
-        ? profile.map((users) => {
-            return (
-              <Grid item xs={12} key={users.id}>
-                <Card>
-                  <div className={style.avatar}>
-                    <img
-                      src={`https://avatars.dicebear.com/v2/avataaars/{{${users.name}}}.svg?options[mood][]=happy`}
-                    />
-                  </div>
-                  <div className={style.details}>
-                    <h2 className={`text-center`}>{users.name}</h2>
-                    <p>Email address: {users.email}</p>
-                    <p>Telephone: {users.phone}</p>
-                    <p>
-                      Address:{" "}
-                      {`${users.address.street} ${users.address.suite} ${users.address.city}`}
-                    </p>
-                    <p>
-                      {" "}
-                      Website:
-                      <a href={`https://${users.website}`}> {users.website}</a>
-                    </p>
-                    <p>Company: {users.company.name}</p>
-                  </div>
-                </Card>
-              </Grid>
-            );
-          })
-        : "loading..."}
+      <Grid container spacing={4}>
+        {loading
+          ? profile.map((users) => {
+              return (
+                <Grid item xs={12} sm={6} key={users.id}>
+                  <Card>
+                    <div className={style.avatar}>
+                      <img
+                        src={`https://avatars.dicebear.com/v2/avataaars/{{${users.name}}}.svg?options[mood][]=happy`}
+                      />
+                    </div>
+                    <div className={style.details}>
+                      <h2 className={`text-center`}>{users.name}</h2>
+                      <p>Email address: {users.email}</p>
+                      <p>Telephone: {users.phone}</p>
+                      <p>
+                        Address:{" "}
+                        {`${users.address.street} ${users.address.suite} ${users.address.city}`}
+                      </p>
+                      <p>
+                        {" "}
+                        Website:
+                        <a href={`https://${users.website}`}>
+                          {" "}
+                          {users.website}
+                        </a>
+                      </p>
+                      <p>Company: {users.company.name}</p>
+                    </div>
+                  </Card>
+                </Grid>
+              );
+            })
+          : "loading..."}
+      </Grid>
     </section>
   );
 };
